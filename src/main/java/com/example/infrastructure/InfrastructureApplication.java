57561bf43b94933a5d18d9cf573ab6ca73615b56
khamd <ogabek.xamdamov@smartbank.uz>
2025-04-22 22:59:25 +0500

    prometheus implemented

--- src/main/java/com/example/infrastructure/InfrastructureApplication.java
+++ src/main/java/com/example/infrastructure/InfrastructureApplication.java
@@ -1,13 +1,25 @@
 package com.example.infrastructure;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class InfrastructureApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(InfrastructureApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(InfrastructureApplication.class, args);
+    }
+
+    @EventListener
+    public void handleEvent(ApplicationReadyEvent event) {
+
+        log.info("Application ready");
     }
 
 }
