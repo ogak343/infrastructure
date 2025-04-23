2c2ac6c7a9edef0edfdfceac2808d500be7008f0
khamd <ogabek.xamdamov@smartbank.uz>
2025-04-23 14:12:43 +0500

    logs implemented

--- src/main/java/com/example/infrastructure/InfrastructureApplication.java
+++ src/main/java/com/example/infrastructure/InfrastructureApplication.java
@@ -4,8 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class InfrastructureApplication {
@@ -14,12 +12,6 @@ public class InfrastructureApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InfrastructureApplication.class, args);
-    }
-
-    @EventListener
-    public void handleEvent(ApplicationReadyEvent event) {
-
-        log.info("Application ready");
     }
 
 }
